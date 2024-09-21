4f8ff9c5deca6d075e79acf67bb2ee8952d904da
Sandvichero <chase.racecar@email.com>
2024-09-21 18:15:22 -0400

    update to use bof.h functions

--- hw1-tests/vm.c
+++ hw1-tests/vm.c
@@ -2,60 +2,53 @@
 #include <string.h>
 #include "bof.h"
 
-
+int parse_bof_header(BOFHeader *header, BOFFILE bf);
 
 int main(int argc, char *argv[]) {
-
-    //arg detector in order to determine if -p is in the args
-    if (strcmp(argv[1],"-p") == 0) {
-        printf("print mode");
-    } else {
-        printf("regular mode");
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [-p] <filename>\n", argv[0]);
+        return -1;
     }
 
-    FILE *file;
-    BOFHeader header;
-    char buffer[256];
+    int print_mode = 0;
+    const char *filename;
 
-    // Open the BOF file
-    file = fopen(argv[1], "rb"); // Open in binary read mode
-    if (file == NULL) {
+    if (strcmp(argv[1], "-p") == 0) {
+        print_mode = 1;
+        filename = argv[2];
+    } else {
+        filename = argv[1];
+    }
+
+    BOFFILE bf = bof_read_open(filename);
+    if (bf.fileptr == NULL) {
         perror("Error opening file");
         return -1;
     }
 
-    // Parse the BOF header
-    if (parse_bof_header(&header, file) != 0) {
-        fclose(file);
+    BOFHeader header;
+    if (parse_bof_header(&header, bf) != 0) {
+        bof_close(bf);
         return -1;
     }
 
-    //Read each line from the file and print it
-        while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        printf("%s", buffer);  // Print each line
+    char buffer[256];
+    while (bof_read_bytes(bf, sizeof(buffer), buffer) > 0) {
+        printf("%s", buffer);
     }
 
-    // Close the file
-    fclose(file);
-
+    bof_close(bf);
     return 0;
 }
 
-int parse_bof_header(BOFHeader *header, FILE *file) {
-    
-    // Read the header from the file, needs to be fixed 
-    if (bof_read_header(header, file) != 0) {
-        fprintf(stderr, "Error reading BOF header\n");
-        return -1;
-    }
+int parse_bof_header(BOFHeader *header, BOFFILE bf) {
+    *header = bof_read_header(bf);
 
-    // Check the magic number, wtf is a magic number anyway
-    if (strncmp(header->magic, "BO32", 4) != 0) {
+    if (!bof_has_correct_magic_number(*header)) {
         fprintf(stderr, "Invalid BOF magic number\n");
         return -1;
     }
 
-    // Check for valid address/length
     if (header->text_start_address < 0 || header->text_length < 0 ||
         header->data_start_address < 0 || header->data_length < 0 ||
         header->stack_bottom_addr < 0) {
@@ -69,7 +62,6 @@ int parse_bof_header(BOFHeader *header, FILE *file) {
         return -1;
     }
 
-    // debug if this code ever works
     printf("BOF Header:\n");
     printf("  Magic Number: %s\n", header->magic);
     printf("  Text Start Address: 0x%08X\n", header->text_start_address);
@@ -78,17 +70,5 @@ int parse_bof_header(BOFHeader *header, FILE *file) {
     printf("  Data Length: %d words\n", header->data_length);
     printf("  Stack Bottom Address: 0x%08X\n", header->stack_bottom_addr);
 
-    return 0; // if successful
+    return 0;
 }
-
-//two modes of operation:
-//1. if(args) = ./vm test.bof - Execution, run a passed bof
-//2. if(args) = ./vm -p test.bof - Print, when -p is passed, print assembly language formed by the bof
-//bof contains a header that needs to be read
-//after header, binary instructions follow
-
-//initializing:
-//memory = 0
-//instructions are loaded, starting at index 0
-//the first N words are filled with instructions from bof, N = text.length
-//
