ba031619b862dfbe79ccd8a29affa82d0f709755
lewsive <phasing.script@gmail.com>
2024-09-23 13:39:16 -0400

    added bof.h and utilities.c functionality, reformatted arg statement

--- hw1-tests/vm.c
+++ hw1-tests/vm.c
@@ -1,33 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+#include "bof.h"
+#include "utilities.c"
+
 
 int main(int argc, char *argv[]) {
 
+    int fileArg;
     //arg detector in order to determine if -p is in the args
     if (strcmp(argv[1],"-p") == 0) {
-        printf("print mode");
+        printf("print mode\n");
+        fileArg = 2;
     } else {
-        printf("regular mode");
+        printf("regular mode\n");
+        fileArg = 1;
     }
-
-    FILE *file;
-    char buffer[256];  // Buffer to store each line
-
-    // Open the file in read mode
-    file = fopen("input.txt", "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        return -1;
-    }
-    printf("dick");
-
-    //Read each line from the file and print it
-        while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        printf("%s", buffer);  // Print each line
-    }
-
-    // Close the file
-    fclose(file);
+    BOFFILE newFile = bof_read_open(argv[fileArg]);
+        printf("reading file: %s\n", newFile.filename);
+    
+    BOFHeader fileHeader = bof_read_header(newFile);
+        printf("data length: %d\n",fileHeader.data_length);
 
     return 0;
 }
@@ -38,10 +30,16 @@ int main(int argc, char *argv[]) {
 //1. if(args) = ./vm test.bof - Execution, run a passed bof
 //2. if(args) = ./vm -p test.bof - Print, when -p is passed, print assembly language formed by the bof
 //bof contains a header that needs to be read
+//information pertaining to the bof needs to be placed in a bof header struct
 //after header, binary instructions follow
+//
 
 //initializing:
 //memory = 0
 //instructions are loaded, starting at index 0
 //the first N words are filled with instructions from bof, N = text.length
-//
+
+//modulation:
+//import appropriate files
+
+
