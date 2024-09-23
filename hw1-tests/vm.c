88b6b3134ac8948df0de5692de2233709ebd2b4d
lewsive <phasing.script@gmail.com>
2024-09-23 16:17:53 -0400

    trimmed redundant statements

--- hw1-tests/vm.c
+++ hw1-tests/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "bof.h"
+#include "utilities.c"
 
 int parse_bof_header(BOFHeader *header, BOFFILE bf);
 
@@ -25,58 +26,19 @@ int main(int argc, char *argv[]) {
 
     // Open the BOF file for reading
     BOFFILE bf = bof_read_open(filename);
-    if (bf.fileptr == NULL) {
-        perror("Error opening file");
-        return -1;
-    }
+    BOFHeader bf_header = bof_read_header(bf);
 
-    // Read the header from the file
-    BOFHeader header;
-    if (parse_bof_header(&header, bf) != 0) {
-        bof_close(bf);
-        return -1;
-    }
-
-    char buffer[256];
-    while (bof_read_bytes(bf, sizeof(buffer), buffer) > 0) {
-        printf("%s", buffer);
-    }
-
-    bof_close(bf);
-    return 0;
-}
-
-int parse_bof_header(BOFHeader *header, BOFFILE bf) {
-    *header = bof_read_header(bf);
-
-    // Check the magic number, wtf is a magic number anyway
-    if (!bof_has_correct_magic_number(*header)) {
-        fprintf(stderr, "Invalid BOF magic number\n");
-        return -1;
-    }
-
-    // Check for valid address/length
-    if (header->text_start_address < 0 || header->text_length < 0 ||
-        header->data_start_address < 0 || header->data_length < 0 ||
-        header->stack_bottom_addr < 0) {
-        fprintf(stderr, "Invalid addresses or lengths in BOF header\n");
-        return -1;
-    }
-
-    if (header->data_start_address <= header->text_start_address ||
-        header->stack_bottom_addr <= header->data_start_address) {
-        fprintf(stderr, "Invalid address relationships in BOF header\n");
-        return -1;
-    }
-
-    // debug if this code works
     printf("BOF Header:\n");
-    printf("  Magic Number: %s\n", header->magic);
-    printf("  Text Start Address: 0x%08X\n", header->text_start_address);
-    printf("  Text Length: %d words\n", header->text_length);
-    printf("  Data Start Address: 0x%08X\n", header->data_start_address);
-    printf("  Data Length: %d words\n", header->data_length);
-    printf("  Stack Bottom Address: 0x%08X\n", header->stack_bottom_addr);
+    printf("  Magic Number: %s\n", bf_header.magic);
+    printf("  Text Start Address: 0x%08X\n", bf_header.text_start_address);
+    printf("  Text Length: %d words\n", bf_header.text_length);
+    printf("  Data Start Address: 0x%08X\n", bf_header.data_start_address);
+    printf("  Data Length: %d words\n", bf_header.data_length);
+    printf("  Stack Bottom Address: 0x%08X\n", bf_header.stack_bottom_addr);
 
     return 0;
 }
+
+
+//We have the data now
+//Next, we need to make the stack
