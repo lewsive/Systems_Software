5010132516b90aa695b9b0013780983aa787ccf2
Sandvichero <chase.racecar@gmail.com>
2024-09-23 15:43:53 -0400

    Update vm.c

--- hw1-tests/vm.c
+++ hw1-tests/vm.c
@@ -1,45 +1,82 @@
 #include <stdio.h>
 #include <string.h>
 #include "bof.h"
-#include "utilities.c"
 
+int parse_bof_header(BOFHeader *header, BOFFILE bf);
 
 int main(int argc, char *argv[]) {
-
-    int fileArg;
-    //arg detector in order to determine if -p is in the args
-    if (strcmp(argv[1],"-p") == 0) {
-        printf("print mode\n");
-        fileArg = 2;
-    } else {
-        printf("regular mode\n");
-        fileArg = 1;
-    }
-    BOFFILE newFile = bof_read_open(argv[fileArg]);
-        printf("reading file: %s\n", newFile.filename);
     
-    BOFHeader fileHeader = bof_read_header(newFile);
-        printf("data length: %d\n",fileHeader.data_length);
+    // Check if the correct number of arguments is provided
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [-p] <filename>\n", argv[0]);
+        return -1;
+    }
 
+    int print_mode = 0;
+    const char *filename;
+
+    // Determine if print mode is enabled
+    if (strcmp(argv[1], "-p") == 0) {
+        print_mode = 1;
+        filename = argv[2];
+    } else {
+        filename = argv[1];
+    }
+
+    // Open the BOF file for reading
+    BOFFILE bf = bof_read_open(filename);
+    if (bf.fileptr == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
+
+    // Read the header from the file
+    BOFHeader header;
+    if (parse_bof_header(&header, bf) != 0) {
+        bof_close(bf);
+        return -1;
+    }
+
+    char buffer[256];
+    while (bof_read_bytes(bf, sizeof(buffer), buffer) > 0) {
+        printf("%s", buffer);
+    }
+
+    bof_close(bf);
     return 0;
 }
 
-////sfdsffvxc
+int parse_bof_header(BOFHeader *header, BOFFILE bf) {
+    *header = bof_read_header(bf);
 
-//two modes of operation:
-//1. if(args) = ./vm test.bof - Execution, run a passed bof
-//2. if(args) = ./vm -p test.bof - Print, when -p is passed, print assembly language formed by the bof
-//bof contains a header that needs to be read
-//information pertaining to the bof needs to be placed in a bof header struct
-//after header, binary instructions follow
-//
+    // Check the magic number, wtf is a magic number anyway
+    if (!bof_has_correct_magic_number(*header)) {
+        fprintf(stderr, "Invalid BOF magic number\n");
+        return -1;
+    }
 
-//initializing:
-//memory = 0
-//instructions are loaded, starting at index 0
-//the first N words are filled with instructions from bof, N = text.length
+    // Check for valid address/length
+    if (header->text_start_address < 0 || header->text_length < 0 ||
+        header->data_start_address < 0 || header->data_length < 0 ||
+        header->stack_bottom_addr < 0) {
+        fprintf(stderr, "Invalid addresses or lengths in BOF header\n");
+        return -1;
+    }
 
-//modulation:
-//import appropriate files
+    if (header->data_start_address <= header->text_start_address ||
+        header->stack_bottom_addr <= header->data_start_address) {
+        fprintf(stderr, "Invalid address relationships in BOF header\n");
+        return -1;
+    }
 
+    // debug if this code works
+    printf("BOF Header:\n");
+    printf("  Magic Number: %s\n", header->magic);
+    printf("  Text Start Address: 0x%08X\n", header->text_start_address);
+    printf("  Text Length: %d words\n", header->text_length);
+    printf("  Data Start Address: 0x%08X\n", header->data_start_address);
+    printf("  Data Length: %d words\n", header->data_length);
+    printf("  Stack Bottom Address: 0x%08X\n", header->stack_bottom_addr);
 
+    return 0;
+}
